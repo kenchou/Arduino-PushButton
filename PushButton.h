c571c542fa14bbe25c61c14d464289e4cc0fa4d1
Ken Chou <kenchou77@gmail.com>
2013-04-11 22:07:02 +0800

    no message

--- PushButton.h
+++ PushButton.h
@@ -6,36 +6,46 @@
 class PushButton
 {
 	public:
-		const int pin;
+		int pin;
 		bool state;	//button state: true/false
 		int pressedValue;	//a value HIGH or LOW to indicate Pressed state
 		unsigned int pushCount;
 	protected:
+		int _pinMode;
 		unsigned int lastReadingState;	//last state read from pin
 		unsigned long lastDebounceTime;  // the last time the output pin was toggled
 		unsigned long debounceDelay;
 	public:
+		/**
+		 * @param int buttonPin
+		 * @param int buttonPinMode INPUT or INPUT_PULLUP
+		 */
 		PushButton(int buttonPin, int buttonPinMode = INPUT);
 		/**
 		 * set a value to check press or release
 		 * @param bool value
 		 */
-		void setPressedValue(bool value = HIGH);
+		void setPressedValue(bool value);
+		bool getPressedValue();
 		/**
 		 * set debounce mode
 		 * @param bool debounce
 		 */
 		void setDebounceMode(bool debounce);
-		void setDebounceThreshold(unsigned long delay);
-		void setInitialState(int value);
+		void setDebounceThreshold(unsigned long delay = 50);
+		/**
+		 * set button state
+		 * default released
+		 */
+		void setState(bool value);
+		bool getState();
 		/**
 		 * read pin state
 		 * @return bool
 		 */
 		bool read();
-		bool isPress();
-		bool getState();
-		void setState(bool state);
+		bool isPressed();
+		bool isReleased();
 };
 
 #endif
